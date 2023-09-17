da7db07475b231515d0d7b5cc6e78158e4f34065
Stephen Kimoi <moenisteve@gmail.com>
2023-09-18 00:41:12 +0300

    solving git conflicting issues

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -3,7 +3,7 @@ import { abi as marketplaceABI} from '../contractABIs/Marketplace.json'
 import {abi as aggregatorABI} from '../contractABIs/aggregatorV3InterfaceABI.json'
 import { abi as priceConsumerABI} from '../contractABIs/PriceConsumerV3.json'; 
 
-export const farmdaoContractAddress = "0x2d7EEc05b7b5eae8bfDd93C988dE87Ac5097Aba6"
+export const farmdaoContractAddress = "0xE3475a69f97269184d0922ea234B6933Ed8E4Af7"
 export const marketplaceContractAddress = "0xe2dEeAECD5A0D7e6E8f55090F4F8521c5373F17F"
 export const aggregatorV3InterfaceAddress = "0x694AA1769357215DE4FAC081bf1f309aDC325306"
 export const priceConsumerV3Address = "0x1ad9d4269E0Ce5b4c30483244c261F31fbD5b6f5"
